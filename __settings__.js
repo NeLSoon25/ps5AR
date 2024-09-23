6e4e6992f0adc622aacca8cd14129002a40f05da
Nelson <nelsoonn25@gmail.com>
2024-09-22 21:25:05 -0600

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2076178.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 196831343, 196831344, 196831367, 196831347, 196831348, 196831349, 196831350, 196831351, 196831353, 196831354, 196831355, 196831356, 196831358, 196831359 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/196831364/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/196831365/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/196831363/1/ammo.js', 'preload' : true},
+];
